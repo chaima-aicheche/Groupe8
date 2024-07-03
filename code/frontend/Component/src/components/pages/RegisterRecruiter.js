780a797030d3a87894527bcac48126662afa0e85
Nouhayla <127697772+NouhaylaElfetehi@users.noreply.github.com>
2024-07-03 16:58:56 +0200

    Forms

---
+++ code/frontend/Component/src/components/pages/RegisterRecruiter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterRecruiterForm from '../forms/RegisterRecruiterForm';
+
+const RegisterRecruiter = () => {
+  return (
+    <div>
+    
+      <RegisterRecruiterForm />
+    </div>
+  );
+};
+
+export default RegisterRecruiter;
