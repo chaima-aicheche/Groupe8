cc0ff7b63555fda163fb7cc10974cf474e292b43
khouloud-kartal <114388142+khouloud-kartal@users.noreply.github.com>
2024-07-11 18:05:57 +0200

    Add Recruiter Offers Cards component

---
+++ code/frontend/App/src/components/OffersRecruiterStats.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import data from '../data/offresStatsRecuiter.json';
+
+const OffersRecruiterStats = () => {
+    return (
+        <div className="dashboard-cards-recruiter">
+            <div className="card">
+                <h3>Total d'Offres Publiées</h3>
+                <p>{data.totalJobs}</p>
+            </div>
+            <div className="card">
+                <h3>Total de Candidatures Reçues</h3>
+                <p>{data.totalApplications}</p>
+            </div>
+            <div className="card">
+                <h3>Candidatures en Cours de Traitement</h3>
+                <p>{data.inProgressApplications}</p>
+            </div>
+            <div className="card">
+                <h3>Candidatures Acceptées</h3>
+                <p>{data.acceptedApplications}</p>
+            </div>
+            <div className="card">
+                <h3>Candidatures Refusées</h3>
+                <p>{data.rejectedApplications}</p>
+            </div>
+        </div>
+    );
+};
+
+export default OffersRecruiterStats;
