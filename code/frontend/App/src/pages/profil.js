201b396425f5f2c7f0ecaf8a0a692f6fd0c8b143
Jordan <121682392+jordan-sissilian@users.noreply.github.com>
2024-07-13 19:43:44 +0200

    .

--- code/frontend/App/src/pages/profil.js
+++ code/frontend/App/src/pages/profil.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import {
     ContainerProfil,
@@ -7,6 +8,13 @@ import {
     InfoProfil,
     BtnNavProfil
 } from '../styles/Profil.style';
+
+const TT = () => {
+
+    return (
+        <p>test</p>
+    );
+}
 
 
 const Profil = () => {
@@ -20,17 +28,27 @@ const Profil = () => {
         </HeaderProfil>
         <NavProfil>
             <BtnNavProfil>
-                <button> A propos </button>
+                <Link to="/aPropos">
+                    <button> A propos </button>
+                </Link>
             </BtnNavProfil>
             <BtnNavProfil>
-                <button> Cv </button>
+                <Link to="/CuriculumVitae">
+                    <button> Cv </button>
+                </Link>
             </BtnNavProfil>
             <BtnNavProfil>
-                <button> A propos de l'entreprise </button>
+                <Link to="/aProposDelEntreprise">
+                    <button> A propos de l'entreprise </button>
+                </Link>
             </BtnNavProfil>
         </NavProfil>
         <InfoProfil>
-            <p> info </p>
+            <Routes>
+                <Route path="/aPropos" element={<TT />} />
+                <Route path="/CuriculumVitae" element={<TT />} />
+                <Route path="/aProposDelEntreprise" element={<TT />} />
+            </Routes>
         </InfoProfil>
     </ContainerProfil>
   );
